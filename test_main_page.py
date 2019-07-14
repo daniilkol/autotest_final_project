945ae7f7023e64cc9ab13d3ecd88f04ab6a3f2dc
daniilkol <52126125+daniilkol@users.noreply.github.com>
2019-07-14 21:26:51 +0300

    Добавил тест для страницы с логином

--- test_main_page.py
+++ test_main_page.py
@@ -1,10 +1,15 @@
 from .pages.main_page import MainPage
 
-link = "http://selenium1py.pythonanywhere.com/"
+#link = "http://selenium1py.pythonanywhere.com/ru/catalogue/the-shellcoders-handbook_209/?promo=newYear"
+link ="http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209?promo=midsummer"
+
+def test_should_exist_login_link(browser):
+    page = MainPage(browser, link)
+    page.open()
+    page.should_be_login_link()
 
 def test_guest_can_go_to_login_page(browser):
     page = MainPage(browser, link)
     page.open()
-    #page.go_to_login_page()
-    page.should_be_login_link()
-
+    login_page = page.go_to_login_page()
+    login_page.should_be_login_page()
