653e2a0ad0463b696d45cf56bfad7056595bac55
daniilkol <52126125+daniilkol@users.noreply.github.com>
2019-07-14 23:09:59 +0300

    Добавил локаторы для страницы с кнгиами

--- pages/locators.py
+++ pages/locators.py
@@ -6,3 +6,10 @@ class MainPageLocators(object):
 class LoginPageLocators(object):
     LOGIN_FORM = (By.NAME, "login_submit")
     REGISTER_FORM = (By.NAME, "registration_submit")
+
+class ProductPageLocators (object):
+    ADD_BUTTON = (By.CSS_SELECTOR, ".btn-add-to-basket")
+    PRODUCT_NAME =(By.CSS_SELECTOR, ".product_main>h1")
+    PRODUCT_PRICE = (By.CSS_SELECTOR, ".product_main>.price_color")
+    SUCCESS_ALERT_WITH_NAME_OF_BOOK =(By.CSS_SELECTOR, ".alert-success:nth-child(1) strong")
+    SUCCESS_ALERT_WITH_PRICE_OF_BASKET =(By.CSS_SELECTOR, ".alert-info strong")
