6461b1b40e9087d72a707df9ca83e7740982b7d6
daniilkol <52126125+daniilkol@users.noreply.github.com>
2019-07-15 21:40:56 +0300

    Переписал тесты с переходом на страницу регистрации, используя второй неявный подход

--- test_main_page.py
+++ test_main_page.py
@@ -1,15 +1,20 @@
 from .pages.main_page import MainPage
+from .pages.login_page import LoginPage
 
 #link = "http://selenium1py.pythonanywhere.com/ru/catalogue/the-shellcoders-handbook_209/?promo=newYear"
-link ="http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209?promo=midsummer"
+#link ="http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209?promo=midsummer"
 
 def test_should_exist_login_link(browser):
-    page = MainPage(browser, link)
+    link = "http://selenium1py.pythonanywhere.com"
+    page=MainPage(browser, link)
     page.open()
     page.should_be_login_link()
 
 def test_guest_can_go_to_login_page(browser):
+    link = "http://selenium1py.pythonanywhere.com"
     page = MainPage(browser, link)
     page.open()
-    login_page = page.go_to_login_page()
+    page.go_to_login_page()
+    login_page = LoginPage(browser, browser.current_url)
     login_page.should_be_login_page()
+
