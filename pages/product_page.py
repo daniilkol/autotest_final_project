710bee55da2effffefb76bc0d137e0f0140d31b6
daniilkol <52126125+daniilkol@users.noreply.github.com>
2019-07-15 21:39:56 +0300

    Добавил методы для тестов в степе номер 6

--- pages/product_page.py
+++ pages/product_page.py
@@ -25,4 +25,16 @@ class ProductPage (BasePage):
         product_alert_price = product_alert.text
         assert product_alert_price == product_price, "Price in success alert and product price does not equal"
 
+    def add_to_basket (self):
+        link = self.browser.find_element(*ProductPageLocators.ADD_TO_BASKET)
+        link.click()
+
+    def should_not_be_success_message_v_1(self):
+        assert self.is_not_element_present(*ProductPageLocators.SUCCESS_MESSAGE), \
+            "Success message is presented, but should not be"
+
+    def should_not_be_success_message_v_2(self):
+        assert self.is_disappeared(*ProductPageLocators.SUCCESS_MESSAGE), \
+            "Success message is presented, but should not be"
+
 
