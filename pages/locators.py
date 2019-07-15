2292bd9b32d1d4ce8dbdc046d9faa270f9789a06
daniilkol <52126125+daniilkol@users.noreply.github.com>
2019-07-15 21:38:28 +0300

    Добавил BasePageLocators

--- pages/locators.py
+++ pages/locators.py
@@ -9,7 +9,13 @@ class LoginPageLocators(object):
 
 class ProductPageLocators (object):
     ADD_BUTTON = (By.CSS_SELECTOR, ".btn-add-to-basket")
-    PRODUCT_NAME =(By.CSS_SELECTOR, ".product_main>h1")
+    PRODUCT_NAME = (By.CSS_SELECTOR, ".product_main>h1")
     PRODUCT_PRICE = (By.CSS_SELECTOR, ".product_main>.price_color")
-    SUCCESS_ALERT_WITH_NAME_OF_BOOK =(By.CSS_SELECTOR, ".alert-success:nth-child(1) strong")
-    SUCCESS_ALERT_WITH_PRICE_OF_BASKET =(By.CSS_SELECTOR, ".alert-info strong")
+    SUCCESS_ALERT_WITH_NAME_OF_BOOK = (By.CSS_SELECTOR, ".alert-success:nth-child(1) strong")
+    SUCCESS_ALERT_WITH_PRICE_OF_BASKET = (By.CSS_SELECTOR, ".alert-info strong")
+    ADD_TO_BASKET = (By.CSS_SELECTOR, ".btn-add-to-basket")
+    SUCCESS_MESSAGE = (By.CSS_SELECTOR, ".alert-success:nth-child(1)")
+
+class BasePageLocators(object):
+    LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
+    LOGIN_LINK_INVALID = (By.CSS_SELECTOR, "#login_link_inc")
