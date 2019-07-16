a5b9cddbf6208c3de7584d40fe8118946749a9f6
daniilkol <52126125+daniilkol@users.noreply.github.com>
2019-07-16 17:16:31 +0300

    Изменил стиль на PEP8

--- pages/base_page.py
+++ pages/base_page.py
@@ -4,62 +4,66 @@ from selenium.webdriver.support import expected_conditions as EC
 import math
 from .locators import BasePageLocators
 
-class BasePage (object):
+class BasePage(object):
 
-    def __init__ (self, browser, url, timeout=10):
-        self.browser = browser
-        self.url = url
+    def __init__ (self,browser,url,timeout=10):
+        self.browser=browser
+        self.url=url
         self.browser.implicitly_wait(timeout)
 
-    def open (self):
+    def open(self):
         self.browser.get(self.url)
 
     def go_to_login_page(self):
-        link = self.browser.find_element(*BasePageLocators.LOGIN_LINK)
+        link=self.browser.find_element(*BasePageLocators.LOGIN_LINK)
         link.click()
 
     def go_to_cart_page(self):
-        link = self.browser.find_element(*BasePageLocators.CART_LINK)
+        link=self.browser.find_element(*BasePageLocators.CART_LINK)
         link.click()
+
+    def should_be_authorized_user(self):
+        assert self.is_element_present(*BasePageLocators.USER_ICON), "User icon is not presented," \
+                                                                     " probably unauthorised user"
 
     def should_be_login_link(self):
         assert self.is_element_present(*BasePageLocators.LOGIN_LINK), "Login link is not presented"
 
-    def is_element_present(self, how, what):
+    def is_element_present(self,how,what):
         try:
-            self.browser.find_element(how, what)
+            self.browser.find_element(how,what)
         except NoSuchElementException:
             return False
         return True
 
     #is_not_element_present: упадет, как только увидит искомый элемент. Не появился: успех, тест зеленый.
-    def is_not_element_present(self, how, what, timeout=4):
+    def is_not_element_present(self,how,what,timeout=4):
         try:
-            WebDriverWait(self.browser, timeout).until(EC.presence_of_element_located((how, what)))
+            WebDriverWait(self.browser,timeout).until(EC.presence_of_element_located((how,what)))
         except TimeoutException:
             return True
 
         return False
 
     #is_disappeared: будет ждать до тех пор, пока элемент не исчезнет.
-    def is_disappeared(self, how, what, timeout=4):
+    def is_disappeared(self,how,what,timeout=4):
         try:
-            WebDriverWait(self.browser, timeout, 1, TimeoutException). \
-                until_not(EC.presence_of_element_located((how, what)))
+            WebDriverWait(self.browser,timeout,1,TimeoutException). \
+                until_not(EC.presence_of_element_located((how,what)))
         except TimeoutException:
             return False
 
         return True
 
     def solve_quiz_and_get_code(self):
-        alert = self.browser.switch_to.alert
-        x = alert.text.split(" ")[2]
-        answer = str(math.log(abs((12 * math.sin(float(x))))))
+        alert=self.browser.switch_to.alert
+        x=alert.text.split(" ")[2]
+        answer=str(math.log(abs((12 * math.sin(float(x))))))
         alert.send_keys(answer)
         alert.accept()
         try:
             #WebDriverWait(self.browser, 3).until(EC.alert_is_present())
-            alert = self.browser.switch_to.alert
+            alert=self.browser.switch_to.alert
             print("Your code: {}".format(alert.text))
             alert.accept()
         except NoAlertPresentException:
