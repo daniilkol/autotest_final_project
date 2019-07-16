300426aa06fe247aec6415537dd977cac43e6483
daniilkol <52126125+daniilkol@users.noreply.github.com>
2019-07-16 15:44:22 +0300

    Добавил тесты корзины со страницы товара

--- test_product_page.py
+++ test_product_page.py
@@ -1,5 +1,6 @@
 from .pages.product_page import ProductPage
 from .pages.login_page import LoginPage
+from .pages.cart_page import CartPage
 import pytest
 import time
 
@@ -30,7 +31,7 @@ def test_guest_cant_see_success_message(browser):
     page=ProductPage(browser, link)
     page.open()
     page.should_not_be_success_message_v_1()
-"""
+
 def test_guest_should_see_login_link_on_product_page(browser):
     link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
     page = ProductPage(browser, link)
@@ -44,3 +45,26 @@ def test_guest_can_go_to_login_page(browser):
     page.go_to_login_page()
     login_page = LoginPage(browser, browser.current_url)
     login_page.should_be_login_page()
+"""
+def test_guest_cant_see_product_in_cart_opened_from_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.go_to_cart_page()
+    cart_page = CartPage(browser, browser.current_url)
+    cart_page.check_cart_is_empty()
+    cart_page.should_be_cart_message()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
