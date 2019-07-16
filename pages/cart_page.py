af2e35a3e7de283f861778af959ee6c2cdc2aae6
daniilkol <52126125+daniilkol@users.noreply.github.com>
2019-07-16 17:17:02 +0300

    Изменил стиль на PEP8

--- pages/cart_page.py
+++ pages/cart_page.py
@@ -2,15 +2,15 @@ from .base_page import BasePage
 from .locators import CartPageLocators
 from selenium.webdriver.common.by import By
 
-class CartPage (BasePage):
+class CartPage(BasePage):
 
     def check_cart_is_empty(self):
         assert self.is_not_element_present(*CartPageLocators.BASKET_SUMMARY), \
             "Your basket is not empty, but should be"
 
     def should_be_cart_message(self):
-        message_link = self.browser.find_element(*CartPageLocators.BASKET_MESSAGE)
-        message = message_link.text
+        message_link=self.browser.find_element(*CartPageLocators.BASKET_MESSAGE)
+        message=message_link.text
         assert "Your basket is empty." in message, \
             "There is no basket empty message, something wrong"
 
