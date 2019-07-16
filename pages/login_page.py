eac1d4d79e01a1a7cb8e481af4da81ddb3f730f1
daniilkol <52126125+daniilkol@users.noreply.github.com>
2019-07-16 17:19:00 +0300

    Добавил метод регистрации на странице регистрации, а также изменил стиль на PEP8

--- pages/login_page.py
+++ pages/login_page.py
@@ -3,13 +3,14 @@ from .locators import LoginPageLocators
 
 
 class LoginPage(BasePage):
+
     def should_be_login_page(self):
         self.should_be_login_url()
         self.should_be_login_form()
         self.should_be_register_form()
 
     def should_be_login_url(self):
-        current_url = self.browser.current_url
+        current_url=self.browser.current_url
         assert "/login" in current_url, "Current url is not correct"
 
     def should_be_login_form(self):
@@ -17,3 +18,32 @@ class LoginPage(BasePage):
 
     def should_be_register_form(self):
         assert self.is_element_present(*LoginPageLocators.REGISTER_FORM), "Register form is not presented on the login url"
+
+    def register_new_user (self,email,password):
+        input_email=self.browser.find_element (*LoginPageLocators.EMAIL_REGISTRATION_FORM)
+        input_email.send_keys(email)
+        input_password=self.browser.find_element (*LoginPageLocators.PASSWORD_REGISTRATION_FORM_1)
+        input_password.send_keys(password)
+        input_password_second_time=self.browser.find_element (*LoginPageLocators.PASSWORD_REGISTRATION_FORM_2)
+        input_password_second_time.send_keys(password)
+        submit_register=self.browser.find_element(*LoginPageLocators.REGISTER_FORM)
+        submit_register.click()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
