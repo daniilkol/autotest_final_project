7cb3fad5e730d63f40684af032ca62ab64eca871
daniilkol <52126125+daniilkol@users.noreply.github.com>
2019-07-16 17:19:35 +0300

    Изменил стиль на PEP8

--- pages/main_page.py
+++ pages/main_page.py
@@ -5,5 +5,5 @@ from selenium.webdriver.common.by import By
 
 class MainPage(BasePage):
 
-    def __init__(self, *args, **kwargs):
-        super(MainPage, self).__init__(*args, **kwargs)
+    def __init__(self,*args,**kwargs):
+        super(MainPage,self).__init__(*args,**kwargs)
