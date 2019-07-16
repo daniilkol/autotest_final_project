82d6bbd662f3fdf54e89171af8ad763c140759be
daniilkol <52126125+daniilkol@users.noreply.github.com>
2019-07-16 17:25:11 +0300

    Переназвал все тесты как требует задание, добавил маркировки, изменил стиль на PEP8

--- test_product_page.py
+++ test_product_page.py
@@ -4,67 +4,61 @@ from .pages.cart_page import CartPage
 import pytest
 import time
 
-#link = "http://selenium1py.pythonanywhere.com/ru/catalogue/coders-at-work_207/"
-#link = "http://selenium1py.pythonanywhere.com/ru/catalogue/the-shellcoders-handbook_209/?promo=newYear"
-#link = "http://selenium1py.pythonanywhere.com/ru/catalogue/coders-at-work_207/?promo=newYear2019"
-"""
-@pytest.mark.parametrize('link', ["http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0",
-                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer1",
-                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer2",
-                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer3",
-                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer4",
-                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer5",
-                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer6",
-                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer7",
-                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer8",
-                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer9"])
-def test_guest_can_add_product_to_cart (browser, link):
-    page = ProductPage(browser, link)
+
+class TestUserAddToCartFromProductPage(object):
+
+    @pytest.fixture(scope="function",autouse=True)
+    def setup(self,browser):
+        link="http://selenium1py.pythonanywhere.com/en-gb/accounts/login/"
+        page=LoginPage(browser,link)
+        page.open()
+        page.register_new_user(email=str(time.time())+"@fakemail.org",password=str(time.time()))
+        page.should_be_authorized_user()
+
+    def test_user_cant_see_success_message(self,browser):
+        link="http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019"
+        page=ProductPage(browser,link)
+        page.open()
+        page.should_not_be_success_message_v_1()
+
+    @pytest.mark.need_review
+    def test_user_can_add_product_to_cart(self,browser):
+        link="http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019"
+        page=ProductPage(browser,link)
+        page.open()
+        page.go_to_alert_after_add_button()
+        page.solve_quiz_and_get_code()
+        page.check_success_alert_about_name()
+        page.check_success_alert_about_price_of_basket()
+
+
+@pytest.mark.need_review
+def test_guest_can_add_product_to_cart(browser):
+    link="http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019"
+    page=ProductPage(browser,link)
     page.open()
-    #page.should_be_add_button() #проверяем есть ли кнопка добавки в корзину
     page.go_to_alert_after_add_button()
     page.solve_quiz_and_get_code()
     page.check_success_alert_about_name()
-    page.check_success_alert_about_price_of_basket ()
+    page.check_success_alert_about_price_of_basket()
 
-def test_guest_cant_see_success_message(browser):
-    page=ProductPage(browser, link)
-    page.open()
-    page.should_not_be_success_message_v_1()
 
-def test_guest_should_see_login_link_on_product_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
-    page = ProductPage(browser, link)
-    page.open()
-    page.should_be_login_link()
-
-def test_guest_can_go_to_login_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
-    page = ProductPage(browser, link)
-    page.open()
-    page.go_to_login_page()
-    login_page = LoginPage(browser, browser.current_url)
-    login_page.should_be_login_page()
-"""
+@pytest.mark.need_review
 def test_guest_cant_see_product_in_cart_opened_from_product_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
-    page = ProductPage(browser, link)
+    link="http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    page=ProductPage(browser,link)
     page.open()
     page.go_to_cart_page()
-    cart_page = CartPage(browser, browser.current_url)
+    cart_page=CartPage(browser,browser.current_url)
     cart_page.check_cart_is_empty()
     cart_page.should_be_cart_message()
 
 
-
-
-
-
-
-
-
-
-
-
-
-
+@pytest.mark.need_review
+def test_guest_can_go_to_login_page_from_product_page(browser):
+    link="http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    page=ProductPage(browser,link)
+    page.open()
+    page.go_to_login_page()
+    login_page=LoginPage(browser,browser.current_url)
+    login_page.should_be_login_page()
